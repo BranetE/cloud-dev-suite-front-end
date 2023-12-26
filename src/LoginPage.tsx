92497d7c795b0b4f772f9024517c974c2e3840a1
BranetE <elatedexorcist@gmail.com>
2023-12-26 03:42:11 +0200

    Initialized repository

---
+++ src/LoginPage.tsx
@@ -0,0 +1,82 @@
+import { Button, Form, Input, Checkbox, Tag } from "antd";
+import "./LoginPage.css";
+
+const onFinish = (values: any) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed = (errorInfo: any) => {
+  console.log("Failed:", errorInfo);
+};
+
+type FieldType = {
+  username?: string;
+  password?: string;
+  remember?: string;
+};
+
+const customizeRequiredMark = (
+  label: React.ReactNode,
+  { required }: { required: boolean }
+) => (
+  <>
+    {required ? (
+      <Tag color="error">Required</Tag>
+    ) : (
+      <Tag color="warning">optional</Tag>
+    )}
+    {label}
+  </>
+);
+
+export function LoginPage(): JSX.Element {
+  return (
+    <Form
+      name="basic"
+      // className="loginForm"
+      layout="vertical"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+      requiredMark={customizeRequiredMark}
+    >
+      <Form.Item<FieldType>
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: "Please input your username!" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item<FieldType>
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Please input your password!" }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item<FieldType>
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{ offset: 8, span: 16 }}
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8 }}>
+        <a>Got no account? Register</a>
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+}
